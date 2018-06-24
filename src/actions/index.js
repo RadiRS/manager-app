9a507e3063daaa7b572ded11a828e727046958ea
Radi <radi.rusadi.s@gmail.com>
2018-06-24 23:01:27 +0700

    Creating a user account

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,9 @@
 import firebase from 'firebase';
 import { 
     EMAIL_CHANGED, 
-    PASSWORD_CHANGED,
-    LOGIN_USER_SUCCESS 
+    PASSWORD_CHANGED, 
+    LOGIN_USER_SUCCESS, 
+    LOGIN_USER_FAIL 
 } from './types';
 
 // Actions create when user type email input
@@ -21,16 +22,32 @@ export const passwordChanged = text => {
     };
 };
 
-// Actions to log in when user press login button and make request to firebase server
+// Actions to log in when user press login button and make request to firebase server and make acount with new user
 export const loginUser = ({ email, password }) => {
-    return (dispatch) => {
-        firebase.auth().signInWithEmailAndPassword( email, password)
-        .then(user => {
-            dispatch({ 
-                type: LOGIN_USER_SUCCESS, 
-                payload: user
+    return dispatch => {
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(email, password)
+            .then(user => loginUserSuccess(dispatch, user))
+            .catch(() => {
+                firebase
+                    .auth()
+                    .createUserWithEmailAndPassword(email, password)
+                    .then(user => loginUserSuccess(dispatch, user))
+                    .catch(() => loginUserFail(dispatch));
             });
-        });
     };
 };
-        
+
+const loginUserFail = (dispatch) => {
+    dispatch({
+        type: LOGIN_USER_FAIL
+    });
+};
+
+const loginUserSuccess = (dispatch, user) => {
+    dispatch({
+        type: LOGIN_USER_SUCCESS,
+        payload: user
+    });
+};
